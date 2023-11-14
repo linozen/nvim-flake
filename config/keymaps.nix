34a0721e06c22df067e7e471359a0256f1c3b515
linozen <linus@sehn.tech>
2023-11-14 16:09:24 +0100

    feat: add more keymaps

--- config/keymaps.nix
+++ config/keymaps.nix
@@ -9,5 +9,38 @@
         silent = true;
       };
     }
+    {
+      mode = "n";
+      key = "<leader>fs";
+      action = "<CMD>w<CR>";
+      options = {
+        silent = true;
+        desc = "Save buffer";
+      };
+    }
+    {
+      mode = "n";
+      key = "<M-q>";
+      action = "<CMD>bd<CR>";
+      options = {
+        silent = true;
+        desc = "Exit buffer";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>qa";
+      action = "<CMD>wqa<CR>";
+      options = {
+        silent = true;
+        desc = "Save and exit";
+      };
+    }
+    {
+      mode = "n";
+      lua = true;
+      action = " function() require('conform').format() end";
+      key = "<leader>cf";
+    }
   ];
 }
