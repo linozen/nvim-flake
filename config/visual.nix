de157df7c7cbd805dd6816b340ee56321aa2eb01
linozen <linus@sehn.tech>
2023-11-15 14:09:58 +0100

    feat: visual refinements

--- config/visual.nix
+++ config/visual.nix
@@ -1,11 +1,41 @@
-{ 
+{pkgs, ...}: {
   # Colorscheme
-  colorschemes.oxocarbon.enable = true; 
+  colorschemes.oxocarbon.enable = true;
   # Buffer line on top
   plugins.bufferline = {
     enable = true;
   };
+  # Status line on bottom
   plugins.lualine = {
     enable = true;
   };
+  # Highlight word under cursor
+  plugins.cursorline = {
+    enable = true;
+    cursorline.enable = false;
+    cursorword.enable = true;
+  };
+  # Highlight cursor
+  extraPlugins = with pkgs.vimPlugins; [
+    specs-nvim
+  ];
+  extraConfigLua = ''
+    require('specs').setup{
+        show_jumps  = true,
+        min_jump = 10,
+        popup = {
+            delay_ms = 0, -- delay before popup displays
+            inc_ms = 5, -- time increments used for fade/resize effects
+            blend = 0, -- starting blend, between 0-100 (fully transparent), see :h winblend
+            width = 100,
+            winhl = "PMenu",
+            fader = require('specs').pulse_fader,
+            resizer = require('specs').shrink_resizer,
+        },
+        ignore_filetypes = {},
+        ignore_buftypes = {
+            nofile = true,
+        },
+    }
+  '';
 }
