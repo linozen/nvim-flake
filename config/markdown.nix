95ba62dfac9f606a6b053818e98475c5672e5d7e
linozen <linus@sehn.tech>
2023-11-21 11:39:40 +0100

    feat: some markdown changes

--- config/markdown.nix
+++ config/markdown.nix
@@ -2,9 +2,17 @@
   plugins.zk = {
     enable = true;
     picker = "telescope";
+    lsp.autoAttach.filetypes = ["markdown" "mdx"];
   };
   plugins.mkdnflow = {
     enable = true;
     links.style = "wiki";
+    filetypes = {
+      md = true;
+      rmd = true;
+      qmd = true;
+      markdown = true;
+      mdx = true;
+    };
   };
 }
