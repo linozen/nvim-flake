dc4faad72819230825e117fc8f781df8e4ef1b82
linozen <linus@sehn.tech>
2023-11-23 16:08:36 +0100

    feat: rework prettier config

--- config/format.nix
+++ config/format.nix
@@ -8,7 +8,14 @@
         # Conform will run multiple formatters sequentially
         python = ["isort" "black"];
         # Use a sub-list to run only the first available formatter
+        json = [["prettierd" "prettier"]];
         javascript = [["prettierd" "prettier"]];
+        javascriptreact = [["prettierd" "prettier"]];
+        typescript = [["prettierd" "prettier"]];
+        typescriptreact = [["prettierd" "prettier"]];
+        html = [["prettierd" "prettier"]];
+        css = [["prettierd" "prettier"]];
+        markdown = [["prettierd" "prettier"]];
         # Use the "*" filetype to run formatters on all filetypes.
         "*" = ["codespell"];
         # Use the "_" filetype to run formatters on filetypes that don't
