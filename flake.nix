aa890ae366ba349d9e073882dc95a4609b8667a2
linozen <linus@sehn.tech>
2023-11-26 00:05:16 +0100

    Change nixvim URL to "github:nix-community/nixvim".

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A nixvim configuration";
 
   inputs = {
-    nixvim.url = "github:linozen/nvim-flake";
+    nixvim.url = "github:nix-community/nixvim";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
